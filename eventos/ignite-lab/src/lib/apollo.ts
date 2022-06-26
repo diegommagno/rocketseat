074f2cbbdb529ed44d3f121b0c1f3465c4196aee
Diego M. Magno <diegomartinelli@live.com>
2022-06-26 23:18:53 +0100

    update-aula-04

--- eventos/ignite-lab/src/lib/apollo.ts
+++ eventos/ignite-lab/src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-eu-west-2.graphcms.com/v2/cl4nzdet304zv01xn562mcv80/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
