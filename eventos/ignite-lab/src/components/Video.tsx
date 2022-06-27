21322e701d418333c9587a3823793f1feb88da71
Diego M. Magno <diegomartinelli@live.com>
2022-06-27 07:16:14 +0100

    update-aula-04

--- eventos/ignite-lab/src/components/Video.tsx
+++ eventos/ignite-lab/src/components/Video.tsx
@@ -91,7 +91,7 @@ export function Video(props: VideoProps) {
 							Comunidade do Discord
 						</a>
 
-						<a href="" className="p-4 text-sm border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
+						<a href="" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
 							<Lightning size={24} />
 							Acesse o desafio
 						</a>
--- eventos/ignite-lab/src/pages/Subscribe.tsx
+++ eventos/ignite-lab/src/pages/Subscribe.tsx
@@ -1,5 +1,6 @@
 import { gql, useMutation } from "@apollo/client";
 import { useState, FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 
 const CREATE_SUBSCRIBER_MUTATION = gql`
@@ -11,20 +12,24 @@ const CREATE_SUBSCRIBER_MUTATION = gql`
 `
 
 export function Subscribe() {
+  const navigate = useNavigate()
+
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
 
-  const [createSubscriber] = useMutation(CREATE_SUBSCRIBER_MUTATION)
+  const [createSubscriber, { loading }] = useMutation(CREATE_SUBSCRIBER_MUTATION)
 
-  function handleSubscribe(event: FormEvent) {
+  async function handleSubscribe(event: FormEvent) {
     event.preventDefault();
 
-    createSubscriber({
+    await createSubscriber({
       variables: {
         name,
         email,
       }
     })
+
+    navigate('/event')
   }
   
   return (
@@ -59,7 +64,8 @@ export function Subscribe() {
 
             <button
               type="submit"
-              className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors"
+              disabled={loading}
+              className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
             >
               Garantir sua vaga
             </button>
