a30bb146cdf2263f46aaa250f69304a325b88693
diegommagno <diegomartinelli@live.com>
2022-11-13 11:14:59 +0000

    explorer-stage05-imc-js

---
+++ explorer/stage-05/calcular-imc/main.js
@@ -0,0 +1,15 @@
+// variables
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  const weight = inputWeight.value
+  const height = inputHeight.value
+
+  console.log(weight, height)
+}
