6b5be39ec3ea67d221d187f13c8b8616fd8b5411
diegommagno <diegomartinelli@live.com>
2023-01-11 04:39:41 +0000

    :sparkles: feat: Adicionar switch de background e selected page

---
+++ explorer/stage-06/spa-universe/README.md
@@ -0,0 +1,39 @@
+<h1 align="center"> SPA Universe </h1>
+
+<p align="center">
+A single page application about the universe. <br/>
+</p>
+
+<p align="center">
+  <a href="#-live-preview">Live Preview</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-technologies">Technologies</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-worked-on">Worked On</a>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+
+
+<p align="center">
+  <img alt="Project photo showing a list of buttons with links." src="./.github/spa-universe-cover.gif" width="90%" />
+</p>
+
+<br/>
+
+## 📝 Live Preview 
+
+- [Brazilian Portuguese](https://diegommagno.com/github/rocketseat/events/devlinks/)
+
+## 🧑🏻‍💻 Technologies
+
+- HTML
+- CSS
+- JavaScript
+
+## 🎓 Worked on
+
+- Creating a router class
+- Modules
+- Switching backgrounds and selected page
--- explorer/stage-06/spa-universe/src/js/index.js
+++ explorer/stage-06/spa-universe/src/js/index.js
@@ -13,3 +13,46 @@ router.handle()
 window.onpopstate = () => router.handle()
 
 window.route = () => router.route()
+
+const home = document.querySelector("#home")
+const universe = document.querySelector("#universe")
+const explore = document.querySelector("#explore")
+
+home.addEventListener("click", () => {
+  home.classList.add("selected")
+  universe.classList.remove("selected")
+  explore.classList.remove("selected")
+  updateSelectedPage()
+  
+})
+
+universe.addEventListener("click", () => {
+  home.classList.remove("selected")
+  universe.classList.add("selected")
+  explore.classList.remove("selected")
+  updateSelectedPage()
+})
+
+explore.addEventListener("click", () => {
+  home.classList.remove("selected")
+  universe.classList.remove("selected")
+  explore.classList.add("selected")
+  updateSelectedPage()
+})
+
+const updateSelectedPage = () => {
+  const body = document.querySelector("body")
+  const page = window.location.pathname
+  if (page === "/") {
+    body.style.background = "url(./src/img/mountains-universe-1.png) no-repeat center center fixed"
+    body.style.backgroundSize = "cover"
+  }
+  if (page === "/explore") {
+    body.style.background = "url(./src/img/mountains-universe-3.png) no-repeat center center fixed"
+    body.style.backgroundSize = "cover"
+  }
+  if (page === "/the-universe") {
+    body.style.background = "url(./src/img/mountains-universe-2.png) no-repeat center center fixed"
+    body.style.backgroundSize = "cover"
+  }
+}
