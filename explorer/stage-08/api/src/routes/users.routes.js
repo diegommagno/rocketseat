d8d71f702fd161c921a75495ce7b62cd03a04d57
diegommagno <contact@diegommagno.com>
2023-01-26 05:43:06 +0000

    :sparkles: feat: Add controllers

--- explorer/stage-08/api/src/routes/users.routes.js
+++ explorer/stage-08/api/src/routes/users.routes.js
@@ -1,12 +1,11 @@
 const { Router } = require("express"); /* importa o express.Router de dentro do express */
 
+const UsersController = require("../controllers/UsersController");
+
 const usersRoutes = Router(); /* inicializar o router */
 
-usersRoutes.post("/", (request, response) => {
-  const { name, email, password } = request.body;
+const usersController = new UsersController(); /* instanciar um controller, criando ele. Necessário isso por ser uma classe */
 
-  response.json({ name, email, password });
-
-});
+usersRoutes.post("/", usersController.create); /* criar um route para acesso ao controller */
 
 module.exports = usersRoutes; /* exporta o router para quem quiser utilizar esse arquivo, pode utilizar. Com isso, o server.js poder acessar as rotas da aplicação */
