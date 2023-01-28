75dde676936a0c7a8c7080744b3848735bc8eceb
diegommagno <contact@diegommagno.com>
2023-01-28 19:01:18 +0000

    :sparkles: feat: Add update users name, email, password support

--- explorer/stage-08/api/src/controllers/UsersController.js
+++ explorer/stage-08/api/src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError"); 
 const sqliteConnection = require("../database/sqlite"); /* Importar a conexão com o database */
 
@@ -19,6 +19,58 @@ class UsersController {
     await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]); /* .run é quando quero executar a inserção no database. (name, email, password) são as colunas que quero inserir dados e [name, email, password] são as informações recebidas do usuário quando ele envia o request */
 
     return response.status(201).json(); /* Se o usuário não existe, criar um novo usuário e retornar o status 201 de criado */
+  }
+
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnection(); /* Fazer a conexão com o database */
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]); /* Testar se encontra o usuário do id passado para o params */
+    /* Aqui, caso o usuário seja encontrado, user vai receber todas as informações de cada coluna da tabela, podendo acessar com usar.id, user.name, etc */
+
+    /* Se o usuário não existe, executar uma exceção com o AppError */
+    if (!user) {
+      throw new AppError("User not found."); 
+    }
+
+    if (!name || !email) {
+      throw new AppError("All fields are required.");
+    }
+
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+    /* Se o email existe && o id de quem tem esse email for diferente do id do usuário, falar que ele já está em uso */
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Email already in use."); /* Se o email já existe, executar uma exceção com o AppError */
+    }
+
+    user.name = name;
+    user.email = email;
+
+    if (password && !old_password) {
+      throw new AppError("Old password is required."); /* Se não passou a senha antiga, precisa fornecer para continuar. */
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password); /* Testar se a senha antiga é igual a senha atual */
+      /* Precisa utilizar o compare para comparar as senhas porque a senha antiga cadastrada (user.password) está criptografada e a digitada (old_password) não */
+
+      if (!checkOldPassword) {
+        throw new AppError("Old password is incorrect."); /* Se a senha antiga não está correta, executar uma exceção (erro) com o AppError */
+      }
+
+      user.password = await hash(password, 8); /* O user vai receber a nova senha criptografada */
+    }
+
+    await database.run(`
+      UPDATE users SET 
+      name = ?, email = ?, password = ?, updated_at = ?
+      WHERE id = ?`,
+      [user.name, user.email, user.password, new Date(), id]
+    );
+    
+    return response.json();
   }
 }
 
--- explorer/stage-08/api/src/routes/users.routes.js
+++ explorer/stage-08/api/src/routes/users.routes.js
@@ -7,5 +7,6 @@ const usersRoutes = Router(); /* inicializar o router */
 const usersController = new UsersController(); /* instanciar um controller, criando ele. Necessário isso por ser uma classe */
 
 usersRoutes.post("/", usersController.create); /* criar um route para acesso ao controller */
+usersRoutes.put("/:id", usersController.update); /* criar um route para acesso ao controller */
 
 module.exports = usersRoutes; /* exporta o router para quem quiser utilizar esse arquivo, pode utilizar. Com isso, o server.js poder acessar as rotas da aplicação */
