1404868b03d689aff09a8cdb9172e5ee2be2949f
diegommagno <contact@diegommagno.com>
2023-02-01 19:40:17 +0000

    :sparkles: feat: Add createNotes migration

---
+++ explorer/stage-08/api/src/database/knex/migrations/20230130192858_createNotes.js
@@ -0,0 +1,16 @@
+exports.up = knex => knex.schema.createTable("notes", table => { 
+/* "notes" é onde coloca o nome da tabela. Up é o processo de criar a tabela. */
+
+  table.increments("id");
+  table.text("title");
+  table.text("description");
+  table.integer("user_id").references("id").inTable("users");
+  /* Crio um campo nessa tabela chamado user_id fazendo referencia a id na tabela users, ou seja, a id do usuário. Com isso, fica obrigatório ter um usuário para criar uma nota e ela fica vinculada ao user. */
+
+  table.timestamp("created_at").default(knex.fn.now());
+  table.timestamp("updated_at").default(knex.fn.now());
+    
+});
+  
+exports.down = knex => knex.schema.dropTable("notes"); 
+/* Down é o processo de deletar a tabela. Troca para dropTable ao invés de createTable e somente passa o nome da tabela. */
