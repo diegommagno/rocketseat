0122da915a5899c3d52908d86198fda4e0b4da59
diegommagno <contact@diegommagno.com>
2023-02-22 19:43:56 +0000

    :sparkles: feat: Create TagsController

---
+++ explorer/stage-08/api/src/routes/tags.routes.js
@@ -0,0 +1,12 @@
+const { Router } = require("express"); /* importa o express.Router de dentro do express */
+
+const TagsController = require("../controllers/TagsController"); /* importa o arquivo notes.routes, então quando chamar notesRouter vai para esse arquivo de grupo de rotas, que tem as rotas de notes */
+
+const tagsRoutes = Router(); /* inicializar o router - aqui é onde vão ficar as rotas */
+
+const tagsController = new TagsController(); /* instanciar o controller, a class tagsController */
+
+tagsRoutes.get("/:user_id", tagsController.index); /* visto que esse é passado através de uma query e não parâmetro, não coloca nada */
+
+
+module.exports = tagsRoutes; /* exporta o router para quem quiser utilizar esse arquivo poder utilizar. Com isso, o server.js poder acessar as rotas da aplicação */
