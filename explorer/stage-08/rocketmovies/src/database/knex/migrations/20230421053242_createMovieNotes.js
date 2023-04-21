5d4e3116e27b1940f21ff08c6ddf4ea2437a8e9b
diegommagno <contact@diegommagno.com>
2023-04-21 09:42:26 +0100

    :sparkles: feat: Add delete on cascade and activate it

--- explorer/stage-08/rocketmovies/src/database/knex/migrations/20230421053242_createMovieNotes.js
+++ explorer/stage-08/rocketmovies/src/database/knex/migrations/20230421053242_createMovieNotes.js
@@ -3,7 +3,7 @@ exports.up = knex => knex.schema.createTable("movie_notes", table => {
   table.text("title");
   table.text("description");
   table.integer("rating");
-  table.integer("user_id").references("id").inTable("users");
+  table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");
 
   table.timestamp("created_at").default(knex.fn.now());
   table.timestamp("updated_at").default(knex.fn.now());
