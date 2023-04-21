51d24f0e9eb9504e3d7d20ffd688b910338b052d
diegommagno <contact@diegommagno.com>
2023-04-21 05:40:25 +0100

    :sparkles: feat: Add update password and datetime in UsersControllers update method

--- explorer/stage-08/rocketmovies/src/controllers/UsersController.js
+++ explorer/stage-08/rocketmovies/src/controllers/UsersController.js
@@ -24,7 +24,7 @@ class UsersController {
   }
 
   async update(request, response) {
-    const { name, email } = request.body;
+    const { name, email, password, old_password } = request.body;
     const { id } = request.params;
 
     const database = await sqliteConnection();
@@ -44,7 +44,28 @@ class UsersController {
     user.name = name ?? user.name; 
     user.email = email ?? user.email;
 
-    await database.run(`UPDATE users SET name = (?), email = (?), updated_at = (?) WHERE id = (?)`, [user.name, user.email, new Date(), user.id]);
+    if (password && !old_password) {
+      throw new AppError("Please, inform the old password to continue."); /* Se não passou a senha antiga, precisa fornecer para continuar. */
+    }
+
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password); /* Testar se a senha antiga é igual a senha atual */
+      /* Precisa utilizar o compare para comparar as senhas porque a senha antiga cadastrada (user.password) está criptografada e a digitada (old_password) não */
+
+      if (!checkOldPassword) {
+        throw new AppError("Old password is incorrect."); /* Se a senha antiga não está correta, executar uma exceção (erro) com o AppError */
+      }
+
+      user.password = await hash(password, 8); /* O user vai receber a nova senha criptografada */
+    }
+
+    await database.run(`
+      UPDATE users SET 
+      name = ?, email = ?, password = ?, updated_at = DATETIME('now')
+      WHERE id = ?`,
+      [user.name, user.email, user.password, id]
+    );
 
     return response.json();
   }
