9775196f954497cdbfab8d4129f94ad66e8ac003
diegommagno <contact@diegommagno.com>
2023-04-21 05:04:21 +0100

    :sparkles: feat: Add hashedPassword on UsersControllers

--- explorer/stage-08/rocketmovies/src/controllers/UsersController.js
+++ explorer/stage-08/rocketmovies/src/controllers/UsersController.js
@@ -1,3 +1,4 @@
+const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 
 const sqliteConnection = require("../database/sqlite");
@@ -15,7 +16,9 @@ class UsersController {
       throw new AppError("Email already in use, please log in.");
     }    
 
-    await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, password]); 
+    const hashedPassword = await hash(password, 8);
+
+    await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]); 
 
     return response.status(201).json();
   }
