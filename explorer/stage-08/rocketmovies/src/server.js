40f975f81c7a86b7cb040350fb3b991a1c756db9
diegommagno <contact@diegommagno.com>
2023-04-21 03:46:52 +0100

    :sparkles: feat: Add sqlite and database

--- explorer/stage-08/rocketmovies/src/server.js
+++ explorer/stage-08/rocketmovies/src/server.js
@@ -1,14 +1,18 @@
 require("express-async-errors");
 
+const database = require("./database/sqlite");
+const AppError = require("./utils/AppError");
+
 const express = require ("express");
 
 const routes = require("./routes");
-const AppError = require("./utils/AppError");
 
 const app = express ();
 app.use(express.json());
 
 app.use(routes);
+
+database();
 
 app.use(( error, request, response, next ) => {
   if (error instanceof AppError) {
