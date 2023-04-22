40d6860af2aeba03bc19d89594b6e6f6cbd61daf
diegommagno <contact@diegommagno.com>
2023-04-22 05:22:20 +0100

    :sparkles: feat: Add movieTagsController movieTags.routes.js

--- explorer/stage-08/rocketmovies/src/routes/movieNotes.routes.js
+++ explorer/stage-08/rocketmovies/src/routes/movieNotes.routes.js
@@ -6,7 +6,6 @@ const movieNotesRoutes = Router();
 
 const movieNotesController = new MovieNotesController();
 
-// Não precisa passar o user_id aqui em index como parâmetro, pois ele já está sendo passado na query.
 movieNotesRoutes.get("/", movieNotesController.index);
 movieNotesRoutes.post("/:user_id", movieNotesController.create);
 movieNotesRoutes.get("/:id", movieNotesController.show);
