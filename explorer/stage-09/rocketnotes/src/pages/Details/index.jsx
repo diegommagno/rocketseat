bfb08a5a0f55aaef85e22f74308affb0b8cc161f
diegommagno <contact@diegommagno.com>
2023-04-25 06:45:39 +0100

    :sparkles: feat: Create Header component

--- explorer/stage-09/rocketnotes/src/pages/Details/index.jsx
+++ explorer/stage-09/rocketnotes/src/pages/Details/index.jsx
@@ -1,15 +1,15 @@
 import { Container } from './styles';
 
+import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
 
 export function Details() {
    return (
       <Container>
+         <Header />
          <h1>Hello World!</h1>
          <span>Diego M. Magno</span>
 
-         <Button title="Entrar" loading/>
-         <Button title="Cadastrar"/>
          <Button title="Voltar"/>
       </Container>
    )
