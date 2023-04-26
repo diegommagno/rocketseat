7d7d7b9dad17811888d953f63407ee10fd816c92
diegommagno <contact@diegommagno.com>
2023-04-26 21:12:33 +0100

    :sparkles: feat: Home: Add NewNote content

--- explorer/stage-09/rocketnotes/src/pages/Home/styles.js
+++ explorer/stage-09/rocketnotes/src/pages/Home/styles.js
@@ -59,4 +59,18 @@ export const Content = styled.div`
 
 export const NewNote = styled.button`
   grid-area: NewNote;
+
+  background-color: ${({ theme }) => theme.COLORS.ORANGE};
+
+  border: none;
+  
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  cursor: pointer;
+
+  svg {
+    margin-right: 8px;
+  }
 `;
