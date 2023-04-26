666960b8c59a9ae8099ad7b95851d1aa4817b9b2
diegommagno <contact@diegommagno.com>
2023-04-26 20:39:03 +0100

    :sparkles: feat: Home: Add menu buttons

--- explorer/stage-09/rocketnotes/src/pages/Home/styles.js
+++ explorer/stage-09/rocketnotes/src/pages/Home/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  height: 100%;
+  height: 100vh;
 
   display: grid;
   grid-template-columns: 250px auto;
@@ -38,6 +38,15 @@ export const Brand = styled.div`
 
 export const Menu = styled.ul`
   grid-area: menu;
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+  padding-top: 64px;
+  text-align: center;
+
+  > li {
+    margin-bottom: 24px;
+  }
 `;
 
 export const Search = styled.div`
