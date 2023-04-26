b55dc192e8c83bc5d359c9e4a47d3f7b3763add7
diegommagno <contact@diegommagno.com>
2023-04-26 09:10:09 +0100

    :sparkles: feat: Create Tag component

---
+++ explorer/stage-09/rocketnotes/src/components/Tags/index.jsx
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+import { Container } from './styles';
+
+export function Tag({ title }) {
+  return (
+    <Container>
+      {title}
+    </Container>
+  );
+}
+
+Tag.propTypes = {
+  title: PropTypes.string.isRequired,
+};
