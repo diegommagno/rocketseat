7bb31d191609c422388278425319a6e5a545e232
diegommagno <contact@diegommagno.com>
2023-04-26 20:01:47 +0100

    :sparkles: feat: Add home grid distribution

--- explorer/stage-09/rocketnotes/src/main.jsx
+++ explorer/stage-09/rocketnotes/src/main.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import GlobalStyles from './styles/global';
 import { ThemeProvider } from 'styled-components';
-import { Details } from './pages/Details/index.jsx';
 
 import theme from './styles/theme';
+
+import { Home } from './pages/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-        <Details />
+        <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
