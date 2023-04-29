861bd0ecbfb78a43424e6b316978f00adf3d5963
diegommagno <contact@diegommagno.com>
2023-04-29 06:32:59 +0100

    :sparkles: feat: Add adjustments on New page

--- explorer/stage-09/rocketnotes/src/pages/New/index.jsx
+++ explorer/stage-09/rocketnotes/src/pages/New/index.jsx
@@ -1,10 +1,11 @@
 import { Container, Form } from './styles';
 
-import { Header } from '../../components/Header';
-import { Input } from '../../components/Input';
 import { Textarea } from '../../components/Textarea';
 import { NoteItem } from '../../components/NoteItem';
 import { Section } from '../../components/Section';
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
 
 export function New() {
   return (
@@ -23,9 +24,18 @@ export function New() {
           <Textarea placeholder="Observações" />
 
           <Section title="Links Úteis">
-            <NoteItem value="playragnarok.com.br"/>
+            <NoteItem value="https://rocketseat.com.br"/>
             <NoteItem isNew placeholder="Novo link" />
           </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="React"/>
+              <NoteItem isNew placeholder="Nova tag" />
+            </div>
+          </Section>
+
+          <Button title="Salvar" />
         </Form>
       </main>
     </Container>
