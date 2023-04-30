746f310e07f68fd1c89aabd0564ca35745c3b9c0
diegommagno <contact@diegommagno.com>
2023-04-30 12:49:53 +0100

    :sparkles: feat: Create component ButtonText

--- explorer/stage-09/rocketmovies/src/pages/MoviePreview/index.jsx
+++ explorer/stage-09/rocketmovies/src/pages/MoviePreview/index.jsx
@@ -3,11 +3,14 @@ import { Container } from './styles';
 import { Header } from '../../components/Header';
 import { Section } from '../../components/Section';
 import { Tag } from '../../components/Tag';
+import { ButtonText } from '../../components/ButtonText';
 
 export function MoviePreview() {
   return (
     <Container>
       <Header />
+
+      <ButtonText title="Excluir nota" />
 
       <Section title="Marcadores">
         <Tag title="express" />
