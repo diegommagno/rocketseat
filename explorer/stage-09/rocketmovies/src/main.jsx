d97a8cba617ef804369772a16fad655c0a94143b
diegommagno <contact@diegommagno.com>
2023-05-28 23:19:08 +0100

    :sparkles: feat: Add CreateMovie page

--- explorer/stage-09/rocketmovies/src/main.jsx
+++ explorer/stage-09/rocketmovies/src/main.jsx
@@ -5,13 +5,13 @@ import GlobalStyles from './styles/global';
 
 import theme from './styles/theme';
 
-import { Profile } from './pages/Profile';
+import { CreateMovie } from './pages/CreateMovie';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Profile />
+      <CreateMovie />
     </ThemeProvider>
   </React.StrictMode>,
 )
