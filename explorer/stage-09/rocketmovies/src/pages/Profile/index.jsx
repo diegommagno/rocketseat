46fac66e9eb4f04e57b966181b7d3ec243042a3a
diegommagno <contact@diegommagno.com>
2023-05-28 21:09:25 +0100

    :sparkles: feat: Add Profile page

--- explorer/stage-09/rocketmovies/src/pages/Profile/index.jsx
+++ explorer/stage-09/rocketmovies/src/pages/Profile/index.jsx
@@ -1,5 +1,8 @@
-import { FiArrowLeft } from 'react-icons/fi';
-import { Container } from './styles';
+import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { Container, Form } from './styles';
+
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
 
 export function Profile() {
   return (
@@ -9,6 +12,34 @@ export function Profile() {
           <FiArrowLeft />
         </a>
       </header>
+
+      <Form>
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+        />
+
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+        />
+
+        <Input
+          placeholder="Senha atual"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Input
+          placeholder="Nova Senha"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button title="Salvar" />
+      </Form>
     </Container>
   );
 }
--- explorer/stage-09/rocketmovies/src/pages/Profile/styles.js
+++ explorer/stage-09/rocketmovies/src/pages/Profile/styles.js
@@ -20,3 +20,8 @@ export const Container = styled.div`
     }
   }
 `;
+
+export const Form = styled.form`
+  max-width: 340px;
+  margin: 30px auto 0;
+`;
