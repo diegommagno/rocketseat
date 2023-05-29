880d5b080defb06c0d75472302ccd6af0ed1acf9
diegommagno <contact@diegommagno.com>
2023-05-29 01:33:23 +0100

    :sparkles: fix: Remove button

--- explorer/stage-09/rocketmovies/src/components/Section/index.jsx
+++ explorer/stage-09/rocketmovies/src/components/Section/index.jsx
@@ -1,17 +1,12 @@
 import PropTypes from 'prop-types';
-import { FiPlus } from 'react-icons/fi';
 
-import { Container, Header, Button } from './styles';
+import { Container, Header } from './styles';
 
 export function Section({ title, children }) {
     return (
         <Container>
             <Header>
                 <h2>{title}</h2>
-                <Button>
-                    <FiPlus />
-                    Adicionar filme
-                </Button>
             </Header>
             {children}
         </Container>
