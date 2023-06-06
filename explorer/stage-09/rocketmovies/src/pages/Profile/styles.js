0712a66bd07d3f77c31fe854fd0b21709316f308
diegommagno <contact@diegommagno.com>
2023-06-06 07:57:39 +0100

    :lipstick: fix: Page Profile fix header bg color

--- explorer/stage-09/rocketmovies/src/pages/Profile/styles.js
+++ explorer/stage-09/rocketmovies/src/pages/Profile/styles.js
@@ -7,7 +7,7 @@ export const Container = styled.div`
     width: 100%;
     height: 144px;
 
-    background: ${({ theme }) => theme.COLORS.BG_900};
+    background: ${({ theme }) => theme.COLORS.BG_RED};
 
     display: flex;
     align-items: center;
