6f4725bfc2cfa2a44aedb620a3daad558f5f8338
diegommagno <contact@diegommagno.com>
2023-06-11 23:48:35 +0100

    :sparkles: feat: Add confetti JS

--- events/boracodar.dev/23-multi-step-form/assets/js/index.js
+++ events/boracodar.dev/23-multi-step-form/assets/js/index.js
@@ -4,7 +4,11 @@ const form03 = document.querySelector('#form-03');
 const btnNextForm01 = document.querySelector('#form-01 #btn-next');
 const btnNextForm02 = document.querySelector('#form-02 #btn-next');
 const btnBackForm02 = document.querySelector('#form-02 #btn-back');
+const btnNextForm03 = document.querySelector('#form-03 #btn-next');
 const btnBackForm03 = document.querySelector('#form-03 #btn-back');
+const canvas = document.querySelector('#confetti');
+
+const jsConfetti = new JSConfetti();
 
 btnNextForm01.addEventListener('click', () => {
   form01.classList.add('hidden');
@@ -17,12 +21,11 @@ btnNextForm02.addEventListener('click', () => {
   form03.classList.remove('hidden');
 });
 
+btnNextForm03.addEventListener('click', () => {
+  jsConfetti.addConfetti();
+});
+
 btnBackForm02.addEventListener('click', () => {
   form02.classList.add('hidden');
   form01.classList.remove('hidden');
-});
-
-btnBackForm03.addEventListener('click', () => {
-  form03.classList.add('hidden');
-  form02.classList.remove('hidden');
 });
