8997b0cb9ac00d39845d47cb8fa8ae28cc417c19
diegommagno <contact@diegommagno.com>
2023-06-12 09:49:11 +0100

    :sparkles: feat: Add extra confetti functions

--- events/boracodar.dev/23-multi-step-form/assets/js/index.js
+++ events/boracodar.dev/23-multi-step-form/assets/js/index.js
@@ -10,6 +10,8 @@ const canvas = document.querySelector('#confetti');
 
 const jsConfetti = new JSConfetti();
 
+let btnNextForm03Count = 0;
+
 btnNextForm01.addEventListener('click', () => {
   form01.classList.add('hidden');
   form02.classList.remove('hidden');
@@ -23,9 +25,24 @@ btnNextForm02.addEventListener('click', () => {
 
 btnNextForm03.addEventListener('click', () => {
   jsConfetti.addConfetti();
+  btnNextForm03Count++;
+  console.log(btnNextForm03Count);
+  if (btnNextForm03Count > 1) {
+    jsConfetti.addConfetti({
+      emojis: ['🚀', '✨', '🚀', '✨', '🚀', '✨'],
+    })
+  }
 });
 
 btnBackForm02.addEventListener('click', () => {
   form02.classList.add('hidden');
   form01.classList.remove('hidden');
 });
+
+btnBackForm03.addEventListener('click', () => {
+  form03.classList.add('hidden');
+  form02.classList.remove('hidden');
+});
+
+
+
