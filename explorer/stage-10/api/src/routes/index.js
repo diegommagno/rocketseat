546bc65c374fcaaba61367d081b1a0802f711be0
diegommagno <contact@diegommagno.com>
2023-06-19 23:47:09 +0100

    :sparkles: feat: Add SessionsController

--- explorer/stage-10/api/src/routes/index.js
+++ explorer/stage-10/api/src/routes/index.js
@@ -3,10 +3,12 @@ const { Router } = require("express"); /* importa o express.Router de dentro do 
 const usersRouter = require("./users.routes"); /* importa o arquivo users.routes, então quando chamar usersRouter vai para esse arquivo de grupo de rotas, que tem as rotas de users */
 const notesRouter = require("./notes.routes"); /* importa o arquivo notes.routes, então quando chamar notesRouter vai para esse arquivo de grupo de rotas, que tem as rotas de notes */
 const tagsRouter = require("./tags.routes"); /* importa o arquivo tags.routes, então quando chamar tagsRouter vai para esse arquivo de grupo de rotas, que tem as rotas de tags */
+const sessionsRouter = require("./sessions.routes"); /* importa o arquivo tags.routes, então quando chamar tagsRouter vai para esse arquivo de grupo de rotas, que tem as rotas de tags */
 
 const routes = Router(); /* inicializar o router - aqui é onde vão ficar as rotas */
 
 routes.use("/users", usersRouter); /* utilizar o arquivo users.routes, então quando chamar usersRouter vai para esse arquivo de grupo de rotas, que tem as rotas de users */
+routes.use("/sessions", sessionsRouter);
 routes.use("/notes", notesRouter); 
 routes.use("/tags", tagsRouter);
 
