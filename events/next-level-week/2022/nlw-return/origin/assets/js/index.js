32302a46aea1ea699f4f3d0f76574dce9e22e1fd
diegommagno <contact@diegommagno.com>
2023-07-08 23:44:16 +0100

    :sparkles: feat: Create onScroll()

---
+++ events/next-level-week/2022/nlw-return/origin/assets/js/index.js
@@ -0,0 +1,9 @@
+const navigation = document.querySelector('#navigation');
+
+function onScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll');
+  } else {
+    navigation.classList.remove('scroll');
+  }
+}
