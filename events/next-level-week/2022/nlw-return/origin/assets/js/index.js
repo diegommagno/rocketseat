aeec70363a3b9f3fe871a89e83645b01403dc15c
diegommagno <contact@diegommagno.com>
2023-07-12 00:29:24 +0100

    :sparkles: feat: Add button .open-menu, .close-menu onclick JS

--- events/next-level-week/2022/nlw-return/origin/assets/js/index.js
+++ events/next-level-week/2022/nlw-return/origin/assets/js/index.js
@@ -7,3 +7,11 @@ function onScroll() {
     navigation.classList.remove('scroll');
   }
 }
+
+function openMenu() {
+  document.body.classList.add('menu-expanded');
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded');
+}
