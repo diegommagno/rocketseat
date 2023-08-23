77569c2d24a53e513d934b741ae84dc65fd33de7
Diego M. Magno <contact@diegommagno.com>
2023-08-23 03:11:25 +0000

    :sparkles: feat: js add newTicket() update img-avatar

--- events/boracodar.dev/33-ticket-generator/assets/js/index.js
+++ events/boracodar.dev/33-ticket-generator/assets/js/index.js
@@ -2,8 +2,15 @@ const input = document.querySelector('.input-wrapper');
 const span = document.querySelector('.ticket-generation .wrapper span');
 const ticketSuccess = document.querySelector('.ticket-success');
 
+const newBgImg = 'https://github.com/diegommagno.png';
+
 const newTicket = () => {
     input.classList.add('hide');
     span.classList.add('hide');
     ticketSuccess.classList.remove('hide');
+
+    const avatarImage = document.querySelector('.ticket #img-avatar');
+    if (avatarImage) {
+        avatarImage.style.background = `url(${newBgImg}) no-repeat center/cover`;
+    }
 }
