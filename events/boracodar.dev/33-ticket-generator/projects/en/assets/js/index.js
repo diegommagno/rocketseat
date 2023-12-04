80c522bffcf4db7e42212fbd98bfa46d07f78faa
diegommagno <contact@diegommagno.com>
2023-12-04 08:39:26 +0000

    :sparkles: Feat: Add English version

---
+++ events/boracodar.dev/33-ticket-generator/projects/en/assets/js/index.js
@@ -0,0 +1,26 @@
+const usernameInput = document.querySelector('.input-wrapper');
+const span = document.querySelector('.ticket-generation .wrapper span');
+const btnTicketSuccess = document.querySelector('.ticket-success');
+
+const newBgImg = 'https://github.com/diegommagno.png';
+
+const newTicket = () => {
+    usernameInput.classList.add('hide');
+    span.classList.add('hide');
+    btnTicketSuccess.classList.remove('hide');
+
+    const avatarImage = document.querySelector('.ticket #img-avatar');
+    if (avatarImage) {
+        avatarImage.style.background = `url(${newBgImg}) no-repeat center/cover`;
+    }
+
+    const updateTicketName = document.querySelector('.name');
+    if (updateTicketName) {
+        updateTicketName.textContent = 'diegommagno';
+    }
+
+    const btnGenerateTicket = document.querySelector('.ticket-generation .wrapper button');
+    if (btnGenerateTicket) {
+        btnGenerateTicket.textContent = 'Fazer Download';
+    }
+}
