03f774e1128e599692e0bf804fb3a042f7e7f7c6
diegommagno <contact@diegommagno.com>
2023-12-04 09:28:32 +0000

    :sparkles: Feat: Update name and photo based on GitHub username input

--- events/boracodar.dev/33-ticket-generator/assets/js/index.js
+++ events/boracodar.dev/33-ticket-generator/assets/js/index.js
@@ -1,26 +1,36 @@
-const usernameInput = document.querySelector('.input-wrapper');
-const span = document.querySelector('.ticket-generation .wrapper span');
-const btnTicketSuccess = document.querySelector('.ticket-success');
+document.addEventListener('DOMContentLoaded', function() {
+    const usernameInput = document.getElementById('username');
+    const span = document.querySelector('.ticket-generation .wrapper span');
+    const btnTicketSuccess = document.querySelector('.ticket-success');
 
-const newBgImg = 'https://github.com/diegommagno.png';
+    const newTicket = () => {
+        // Retrieve the input value
+        const usernameInputContent = usernameInput.value;
 
-const newTicket = () => {
-    usernameInput.classList.add('hide');
-    span.classList.add('hide');
-    btnTicketSuccess.classList.remove('hide');
+        // Check if the username is not empty
+        if (usernameInputContent.trim() !== '') {
+            usernameInput.parentNode.classList.add('hide');
+            span.classList.add('hide');
+            btnTicketSuccess.classList.remove('hide');
 
-    const avatarImage = document.querySelector('.ticket #img-avatar');
-    if (avatarImage) {
-        avatarImage.style.background = `url(${newBgImg}) no-repeat center/cover`;
+            const avatarImage = document.querySelector('.ticket #img-avatar');
+            if (avatarImage) {
+                avatarImage.style.background = `url(https://github.com/${usernameInputContent}.png) no-repeat center/cover`;
+            }
+
+            const updateTicketName = document.querySelector('.name');
+            if (updateTicketName) {
+                updateTicketName.textContent = usernameInputContent;
+            }
+
+            const btnGenerateTicket = document.querySelector('.ticket-generation .wrapper button');
+            if (btnGenerateTicket) {
+                btnGenerateTicket.textContent = 'Download Ticket';
+            }
+        }
     }
 
-    const updateTicketName = document.querySelector('.name');
-    if (updateTicketName) {
-        updateTicketName.textContent = 'diegommagno';
-    }
-
-    const btnGenerateTicket = document.querySelector('.ticket-generation .wrapper button');
-    if (btnGenerateTicket) {
-        btnGenerateTicket.textContent = 'Fazer Download';
-    }
-}
+    // Add event listener to the button
+    const btnGenerateTicket = document.getElementById('generateTicket');
+    btnGenerateTicket.addEventListener('click', newTicket);
+});
