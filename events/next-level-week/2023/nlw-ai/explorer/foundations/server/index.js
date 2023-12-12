08c890023e4e2cee826be4ef0466c69d20d41419
diegommagno <contact@diegommagno.com>
2023-12-12 14:19:28 +0000

    :sparkles: Feat: Get video ID

--- events/next-level-week/2023/nlw-ai/explorer/foundations/server/index.js
+++ events/next-level-week/2023/nlw-ai/explorer/foundations/server/index.js
@@ -4,8 +4,8 @@ import express from "express";
 const app = express(); // Initiate the application. Pega o express e coloca ele dentro da constant app para poder usar ela.
 app.use(cors()); // Use the cors library.
 
-app.get("/summary", (request, response) => {
-    response.send("Hello World!")
+app.get("/summary/:id", (request, response) => {
+    response.send("Video ID:" + request.params.id)
 }); // Request tem todas as informações da requisição que foi feita para o servidor. Response vai ser usado para devolver uma resposta para quem fez a requisição.
 
 app.listen(3333, () => console.log("Server is running on port 3333.")); // Listen to the port 3333. Express é o porteiro. Ele que vai receber as requisições e devolver as respostas. O node é o motor que vai fazer o processamento das requisições e respostas. Digitar node --watch server/index.js para rodar o servidor, --watch é para que sempre que haja uma alteração no código, ele reiniciar o servidor para usar o novo código do projeto.
