69bbb611e749ae2cfaabd8a149cdf292bde53c14
diegommagno <contact@diegommagno.com>
2023-12-12 23:05:45 +0000

    :sparkles: Feat: Add form.js

---
+++ events/next-level-week/2023/nlw-ai/explorer/foundations/web/form.js
@@ -0,0 +1,20 @@
+import { server } from './server.js';
+
+const form = document.querySelector('#form');
+const input = document.querySelector('#url');
+const content = document.querySelector('#content');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const videoURL = input.value;
+
+    if(!videoURL.includes('shorts')){
+        return content.innerHTML = `Please, enter the URL of a YouTube Short video.`
+    }
+
+    const params = videoURL.split('shorts/')[1];
+    const videoID = params.split('?si')[0]; // const [videoID] = params.split('?si') would also work.
+
+    content.innerHTML = 'Obtaining text from audio...'
+});
