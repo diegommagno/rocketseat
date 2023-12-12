37ef417d79f0218afb74645fb6d067f81889c8b0
diegommagno <contact@diegommagno.com>
2023-12-12 15:26:41 +0000

    :sparkles: Feat: Update response from app.get with response.json

--- events/next-level-week/2023/nlw-ai/explorer/foundations/server/index.js
+++ events/next-level-week/2023/nlw-ai/explorer/foundations/server/index.js
@@ -8,7 +8,7 @@ app.use(cors()); // Use the cors library.
 
 app.get("/summary/:id", (request, response) => {
     download(request.params.id); // Provide the download function with the video id.
-    response.send("Video ID:" + request.params.id)
+    response.json({ message: "Download started." }); // Return a JSON response.
 }); // Request tem todas as informações da requisição que foi feita para o servidor. Response vai ser usado para devolver uma resposta para quem fez a requisição.
 
 app.listen(3333, () => console.log("Server is running on port 3333.")); // Listen to the port 3333. Express é o porteiro. Ele que vai receber as requisições e devolver as respostas. O node é o motor que vai fazer o processamento das requisições e respostas. Digitar node --watch server/index.js para rodar o servidor, --watch é para que sempre que haja uma alteração no código, ele reiniciar o servidor para usar o novo código do projeto.
