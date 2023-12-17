73b8d49c8b5fd26c7d8a97edc17e46c976000180
diegommagno <contact@diegommagno.com>
2023-12-17 23:09:41 +0000

    :sparkles: Feat: Add cors package for handling cross-origin resource sharing (CORS)

--- explorer/stage-10/api/src/server.js
+++ explorer/stage-10/api/src/server.js
@@ -1,15 +1,16 @@
 require("express-async-errors");
-
 const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
 const uploadConfig = require("./configs/upload");
 
+const cors = require("cors");
 const express = require('express'); /* import express */
 const routes = require("./routes"); /* importa o arquivo index.js, então quando chamar routes vai para esse arquivo de grupo de rotas, que tem os arquivos de grupo de rotas */
 
 migrationsRun();
 
 const app = express(); /* inicializar o express, iniciar a API. O app é a nossa API, então app.get é buscar informações na API. */
+app.use(cors());
 app.use(express.json()); /* utilizar o express.json() para receber dados JSON */
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
