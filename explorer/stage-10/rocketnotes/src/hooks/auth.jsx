9b7d89fe7ff7a4837c8271486afc395d439872ee
diegommagno <contact@diegommagno.com>
2023-12-18 21:51:11 +0000

    :sparkles: Feat: Add AuthProvider and useAuth hook

---
+++ explorer/stage-10/rocketnotes/src/hooks/auth.jsx
@@ -0,0 +1,28 @@
+import { createContext, useContext } from 'react'; // useContext to use the Context API.
+import PropTypes from 'prop-types';
+
+const AuthContext = createContext({});
+
+function AuthProvider({ children }) {
+    return (
+        <AuthContext.Provider value={{ email: 'diego@email.com' }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+// Children is the child of AuthProvider, which is <Routes /> in main.jsx, containing all the application routes.
+
+function useAuth() {
+    const context = useContext(AuthContext);
+
+    return context;
+
+    // This is the hook, so it's named useAuth.
+}
+
+AuthProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export { AuthProvider, useAuth };
--- explorer/stage-10/rocketnotes/src/pages/SignIn/index.jsx
+++ explorer/stage-10/rocketnotes/src/pages/SignIn/index.jsx
@@ -1,8 +1,7 @@
-import { useContext } from 'react'; // This allows us to use the Context API.
 import { FiMail, FiLock } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
-import { MyContext } from '../../myContext';
+import { useAuth } from '../../hooks/auth';
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
@@ -11,7 +10,7 @@ import { Container, Form, Background } from './styles';
 
 export function SignIn() {
 
-  const data = useContext(MyContext);
+  const data = useAuth();
   console.log("Meu contexto: ", data)
 
   return (
