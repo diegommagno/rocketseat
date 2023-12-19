bf5f50c855bdb928da363d3b1345b3b008116658
diegommagno <contact@diegommagno.com>
2023-12-19 22:35:15 +0000

    :sparkles: Feat: Update authentication functionality and add sign-in feature

--- explorer/stage-10/rocketnotes/src/hooks/auth.jsx
+++ explorer/stage-10/rocketnotes/src/hooks/auth.jsx
@@ -1,11 +1,36 @@
-import { createContext, useContext } from 'react'; // useContext to use the Context API.
+import { createContext, useContext, useState } from 'react'; // useContext to use the Context API.
+
+import { api } from '../services/api';
+
 import PropTypes from 'prop-types';
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
+    // Authentication function
+
+    // States are usually created in the beginning of the function
+    const [data, setData] = useState({});
+
+    async function signIn({ email, password }) {
+        try {
+            const response = await api.post("/sessions", { email, password});
+            const { user, token } = response.data;
+
+            api.defaults.headers.authorization = `Bearer ${token}`; // Estou inserindo um token do estilo bearer em todas as requisições que o usuário vai fazer a partir de agora.
+            setData({ user, token });
+
+        } catch (error) {
+            if(error.response) {
+                alert(error.response.data.message);
+            } else {
+                alert("It was not possible to sign in. Please try again.");
+            }
+          }
+        }
+
     return (
-        <AuthContext.Provider value={{ email: 'diego@email.com' }}>
+        <AuthContext.Provider value={{ signIn, user: data.user }}>
             {children}
         </AuthContext.Provider>
     );
--- explorer/stage-10/rocketnotes/src/routes/index.jsx
+++ explorer/stage-10/rocketnotes/src/routes/index.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter } from 'react-router-dom';
+import { useAuth } from '../hooks/auth';
 
 import { AppRoutes } from './app.routes';
 import { AuthRoutes } from './auth.routes';
 
 export function Routes() {
+  const { user } = useAuth();
+
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      {user ? <AppRoutes /> : <AuthRoutes />} 
     </BrowserRouter>
   )
 }
+
+// If the user is logged in, the AppRoutes component is rendered, otherwise the AuthRoutes component is rendered.
