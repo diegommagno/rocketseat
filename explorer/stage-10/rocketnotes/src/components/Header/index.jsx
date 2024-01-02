0b0d54808a28ccdcf7273da98c04ce37ac5782ca
diegommagno <contact@diegommagno.com>
2024-01-02 00:02:46 +0000

    :sparkles: Feat: Update Header component to display new user avatar

--- explorer/stage-10/rocketnotes/src/components/Header/index.jsx
+++ explorer/stage-10/rocketnotes/src/components/Header/index.jsx
@@ -1,15 +1,24 @@
 import { RiShutDownLine } from 'react-icons/ri';
 import { useAuth } from '../../hooks/auth';
 
+import { api } from '../../services/api';
+
+import avatarPlaceHolder from '../../assets/images/avatar-placeholder.svg';
+
 import { Container, Profile, Logout } from './styles';
 
 export function Header() {
-    const { signOut } = useAuth();
+    const { signOut, user } = useAuth();
+
+    const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceHolder;
 
     return (
         <Container>
             <Profile to="/profile">
-                <img src="https://github.com/diegommagno.png" alt="Foto do usuário" />
+                <img 
+                    src={avatarUrl}
+                    alt="Foto do usuário" 
+                />
                 <div>
                     <span>Bem-vindo,</span>
                     <strong>Diego M. Magno</strong>
