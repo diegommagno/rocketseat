7c7ee9710d195f063561b884e45bf2976674d7c6
diegommagno <contact@diegommagno.com>
2024-01-08 22:38:52 +0000

    :sparkles: Feat: Load note details from database

--- explorer/stage-10/rocketnotes/src/pages/Details/index.jsx
+++ explorer/stage-10/rocketnotes/src/pages/Details/index.jsx
@@ -1,4 +1,10 @@
+import { useState, useEffect } from 'react';
+
+import { api } from '../../services/api';
+
 import { Container, Links, Content } from './styles';
+
+import { useParams, useNavigate } from 'react-router-dom';
 
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button'; 
@@ -7,36 +13,80 @@ import { Tag } from '../../components/Tag';
 import { ButtonText } from '../../components/ButtonText';
 
 export function Details() {
+   const [data, setData] = useState(null);
+
+   const params = useParams();
+   const navigate = useNavigate();
+
+   function handleBack() {
+      navigate("/");
+   }
+
+   useEffect(() => {
+      async function fetchNote() {
+         const response = await api.get(`/notes/${params.id}`);
+         setData(response.data);
+      }
+
+      fetchNote();
+   }, []);
+
    return (
       <Container>
          <Header />
 
-         <main>
-            <Content>
-               <ButtonText title="Excluir nota" />
+         {
+            data &&
+            <main>
+               <Content>
+                  <ButtonText title="Excluir nota" />
 
-               <h1>Introdução ao React</h1>
+                  <h1>{data.title}</h1>
 
-               <p>
-                  O React é uma biblioteca JavaScript de código aberto amplamente utilizada para criar interfaces de usuário em aplicativos web e móveis. Foi criado pelo Facebook em 2011 e tornou-se rapidamente popular entre os desenvolvedores devido à sua facilidade de uso, desempenho e capacidade de criar componentes reutilizáveis. O React permite que os desenvolvedores criem aplicativos com uma interface de usuário dinâmica e responsiva que pode ser atualizada sem a necessidade de recarregar a página, proporcionando aos usuários uma experiência mais rápida e interativa. Além disso, o React possui uma grande comunidade de desenvolvedores que contribuem com bibliotecas, ferramentas e recursos, tornando-o uma das escolhas mais populares para o desenvolvimento de aplicativos modernos.
-               </p>
+                  <p>
+                     {data.description}
+                  </p>
 
-               <Section title="Links Úteis">
-                  <Links>
-                     <li><a href="#">https://www.rocketseat.com.br</a></li>
-                     <li><a href="#">https://www.rocketseat.com.br</a></li>
-                  </Links>
-               </Section>
+                  {  
+                     data.links &&
+                     <Section title="Links Úteis">
+                        <Links>
+                        {
+                           data.links.map(link => (
+                              <li key={String(link.id)}>
+                                 <a href={link.url} target="_blank" rel="noreferrer">
+                                    {link.url}
+                                 </a>
+                              </li>
+                           ))
+                        }
+                        </Links>
+                     </Section>
+                  }
 
-               <Section title="Marcadores">
-                  <Tag title="express" />
-                  <Tag title="node.js" />
-               </Section>
+                  {
+                     data.tags &&
+                     <Section title="Marcadores">
+                        {
+                           data.tags.map(tag => (
+                              <Tag
+                                 key={String(tag.id)}
+                                 title={tag.name} 
+                              />
+                           ))
+                        }
+                     </Section>
+                  }
 
-               <Button title="Voltar"/>
+                  <Button 
+                     title="Voltar" 
+                     onClick={handleBack}
+                  />
 
-            </Content>
-         </main>
+               </Content>
+            </main>
+         }
+
       </Container>
    )
 }
