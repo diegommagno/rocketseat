34fd13b4b79138db268bbfd94ce2510b9112e69b
diegommagno <contact@diegommagno.com>
2024-01-17 18:45:30 +0000

    :sparkles: Feat: Add conditional rendering for unauthorized users

--- explorer/stage-10/rocketnotes/src/routes/auth.routes.jsx
+++ explorer/stage-10/rocketnotes/src/routes/auth.routes.jsx
@@ -4,12 +4,15 @@ import { SignIn } from '../pages/SignIn';
 import { SignUp } from '../pages/SignUp';
 
 export function AuthRoutes() {
+  const user = localStorage.getItem("@rocketnotes:user");
+
  return (
   <Routes>
     <Route path="/" element={<SignIn />} />
     <Route path="/register" element={<SignUp />} />
 
-    <Route path="*" element={<Navigate to="/" />} />
+    { !user && <Route path="*" element={<Navigate to="/" />} /> } 
+    {/* Somente execute isso se o usuário for nulo, portanto, não estiver logado. Assim o user pode colocar a nota nos favoritos e voltar nela, caso logado. */}
   </Routes>
  ) 
 }
