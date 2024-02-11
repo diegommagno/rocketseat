204e2d629ad1e72d546a4e7a237d4291e4d049b6
Diego Magno <contact@diegommagno.com>
2024-02-11 23:56:33 +0000

    :sparkles: Feat: Add logo and header

--- events/next-level-week/2024/nlw-expert/react-js/nlw-expert-notes/src/app.tsx
+++ events/next-level-week/2024/nlw-expert/react-js/nlw-expert-notes/src/app.tsx
@@ -1,7 +1,19 @@
+import logo from './assets/logo-nlw-expert.svg'
+
 export function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt='NLW Expert'/>
+      
+      <form className='w-full'>
+        <input 
+          type="text" 
+          placeholder='Busque em suas notas...' 
+          className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder: text-slate-500'
+        />
+      </form>
+      
+      <div className='h-px bg-slate-700'/>
+    </div>
   )
 }
