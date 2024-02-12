a64c5d8d5b523b73f2fb840f4a2096131f22d44e
Diego Magno <contact@diegommagno.com>
2024-02-12 23:24:01 +0000

    :sparkles: Feat: Add cards

--- events/next-level-week/2024/nlw-expert/react-js/nlw-expert-notes/src/app.tsx
+++ events/next-level-week/2024/nlw-expert/react-js/nlw-expert-notes/src/app.tsx
@@ -14,6 +14,30 @@ export function App() {
       </form>
       
       <div className='h-px bg-slate-700'/>
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <div className="rounded-md bg-slate-700 p-5 space-y-3">
+          <span className="text-sm font-medium text-slate-200">Adicionar nota</span>
+          <p className="text-sm leading-6 text-slate-400">
+            Grave uma nota em áudio que será convertida para texto automaticamente.
+          </p>
+        </div>
+
+        <div className="rounded-md bg-slate-800 p-5 space-y-3">
+          <span className="text-sm font-medium text-slate-300">há 2 dias</span>
+          <p className="text-sm leading-6 text-slate-400">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi porro earum laboriosam facilis vitae? Ratione illo cum ab dolorum commodi ad architecto laborum hic deserunt quos. Rerum repellat adipisci aperiam?
+          </p>
+        </div>
+
+        <div className="rounded-md bg-slate-800 p-5 space-y-3">
+          <span className="text-sm font-medium text-slate-300">há 4 dias</span>
+          <p className="text-sm leading-6 text-slate-400">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae aperiam necessitatibus qui quas doloribus itaque incidunt ipsum quaerat at non placeat atque aliquid saepe, ea aspernatur quod odit ut voluptas!
+          </p>
+        </div>
+
+      </div>
     </div>
   )
 }
