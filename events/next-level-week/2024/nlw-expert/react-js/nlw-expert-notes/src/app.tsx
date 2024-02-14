a1a863113a24b5e7d515754c7512684fdb6ef957
Diego Magno <contact@diegommagno.com>
2024-02-14 19:58:10 +0000

    :sparkles: Feat: Add search bar functionality

--- events/next-level-week/2024/nlw-expert/react-js/nlw-expert-notes/src/app.tsx
+++ events/next-level-week/2024/nlw-expert/react-js/nlw-expert-notes/src/app.tsx
@@ -1,7 +1,7 @@
 import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 
 interface Note {
   id: string
@@ -10,6 +10,7 @@ interface Note {
 }
 
 export function App() {
+  const [search, setSearch] = useState('')
   const [notes, setNotes] = useState<Note[]>(() => {
     const notesOnStorage = localStorage.getItem('notes');	// Vou ver se no localStorage tenho um item chamado notes
 
@@ -35,6 +36,16 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray)) // Converter o array em texto porque o localStorage não aceita arrays
   }
 
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value
+
+    setSearch(query)
+  }
+
+  const filteredNotes = search !== ''
+    ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+    : notes
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt='NLW Expert'/>
@@ -44,6 +55,7 @@ export function App() {
           type="text" 
           placeholder='Busque em suas notas...' 
           className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder: text-slate-500'
+          onChange={handleSearch}
         />
       </form>
       
@@ -52,7 +64,7 @@ export function App() {
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard onNoteCreated={onNoteCreated}/>
 
-        {notes.map(note => {
+        {filteredNotes.map(note => {
           return <NoteCard key={note.id} note={note} />
         })}
       </div>
